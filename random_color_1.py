3669bf640238cc45c75796dc46b630ada656adee
achrafoo2018 <43815739+achrafoo2018@users.noreply.github.com>
2019-02-04 16:31:15 +0100

    Create random_color_1.py
    This program make a beautiful hexagonal with random colored h

---
+++ random_color_1.py
@@ -0,0 +1,14 @@
+import turtle
+from random import randint
+
+colors = ['blue','purple','red','yellow','orange','green']
+tr = turtle.Pen()
+tr.speed(100)
+turtle.bgcolor("black")
+for x in range(330):
+        tr.pencolor(colors[randint(0,5)])
+        tr.width(x/100+1)
+        tr.forward(x)
+        tr.left(300)
+        
+turtle.done()
