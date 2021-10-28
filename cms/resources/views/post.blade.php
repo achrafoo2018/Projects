609078362f467c58b5462b52c25b601b80259392
achrafoo2018 <achrafb.s2015@gmail.com>
2021-10-28 21:19:24 +0100

    delete cms

--- cms/resources/views/post.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title')
-    <title>Post</title>
-@stop
-@section('content')
-    <h1>Post {{$result->title}}</h1>
-    <p>Content: {{$result->content}}</p>
-@stop
